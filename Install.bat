42f47f83cba286946100eb1a45d94b6bee341cef
Joseloll <106576578+Joseloll@users.noreply.github.com>
2023-02-11 12:49:06 -0800

    Delete Install.bat

--- Install.bat
+++
@@ -1,8 +0,0 @@
-pip install pillow
-pip install pystyle
-pip install requests
-pip install browser_cookie3
-pip install pyinstaller
-pip install opencv-python
-pip install psutil
-pip install browser_history
